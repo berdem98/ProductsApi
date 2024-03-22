480a9dfa3e6196dbdef0f923ad5ec6b664895e7d
Berk Erdem <127400459+berdem98@users.noreply.github.com>
2024-03-22 15:55:21 +0300

    Login-Create

--- ProductsApi/Controllers/ProductsController.cs
+++ ProductsApi/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ProductsApi.DTO;
 using ProductsApi.Models;
 
 namespace ProductsApi.Controllers
@@ -18,7 +19,8 @@ namespace ProductsApi.Controllers
         [HttpGet]
         public async Task<IActionResult> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _context.Products.Where(i => i.IsActive).Select(p => ProductToDTO(p)
+            ).ToListAsync();
 
             return Ok(products);
         }
@@ -27,25 +29,29 @@ namespace ProductsApi.Controllers
         {
             if (id == null)
                 return NotFound();
-            var product = await _context.Products.FirstOrDefaultAsync(i=>i.ProductId == id);
+            var product = await _context.Products
+                .Where(p=>p.ProductId==id)
+                .Select(p =>
+                ProductToDTO(p)
+                ).FirstOrDefaultAsync();
             if (product == null)
                 return NotFound();
             return Ok(product);
-            
+
         }
         [HttpPost]
         public async Task<IActionResult> CreateProduct(Product product)
         {
             _context.Products.Add(product);
             await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetProduct), new {id=product.ProductId}, product);
+            return CreatedAtAction(nameof(GetProduct), new { id = product.ProductId }, product);
         }
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateProduct(int id, Product product)
         {
-            if(id != product.ProductId)
+            if (id != product.ProductId)
                 return BadRequest();
-            var p = await _context.Products.FirstOrDefaultAsync(p=>p.ProductId == id);
+            var p = await _context.Products.FirstOrDefaultAsync(p => p.ProductId == id);
             if (p == null)
                 return NotFound();
             p.ProductName = product.ProductName;
@@ -61,6 +67,36 @@ namespace ProductsApi.Controllers
                 return NotFound();
             }
             return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int? id)
+        {
+            if (id == null) return NotFound();
+            var product = await _context.Products.FirstOrDefaultAsync(i => i.ProductId == id);
+            if (product == null) return NotFound();
+            _context.Products.Remove(product);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                return NotFound();
+            }
+            return NoContent();
+
+
+        }
+
+        private static ProductDTO ProductToDTO(Product p)
+        {
+            return new ProductDTO
+            {
+                ProductId = p.ProductId,
+                ProductName = p.ProductName,
+                Price = p.Price,
+            };
         }
     }
 }
--- ProductsApi/Migrations/20240321185749_IdentityTablesAdd.Designer.cs
+++ ProductsApi/Migrations/20240321185749_IdentityTablesAdd.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductsApi.Models;
 
@@ -10,9 +11,11 @@ using ProductsApi.Models;
 namespace ProductsApi.Migrations
 {
     [DbContext(typeof(ProductsContext))]
-    partial class ProductsContextModelSnapshot : ModelSnapshot
+    [Migration("20240321185749_IdentityTablesAdd")]
+    partial class IdentityTablesAdd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.17");
